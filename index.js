f9bdd29d114f51730951b58f6561e7eed40bf4f5
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2024-06-21 16:23:07 +0530

    variable changes

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-const express = require('express');
-const axios = require('axios'); 
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 app.use(cors());
 
+app.get("/latest-news", async (req, res) => {
+  try {
+    const apiKey = "6ee8d5e3d8dc4db9a8e5b257929a41a1";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
 
+    if (req.query.category) {
+        url += `&category=${req.query.category.toLowerCase()}`;
+      }
 
-app.get('/latest-news', async (req, res) => {
-    try {
-        const apiKey = "6ee8d5e3d8dc4db9a8e5b257929a41a1";
-        const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
+    // Fetch data from NewsAPI
+    const response = await axios.get(url);
+    console.log(response);
 
-        if (req.query.category) {
-            url += `&category=${req.query.category.toLowerCase()}`;
-        }
-        
-        // Fetch data from NewsAPI
-        const response = await axios.get(url);
-        console.log(response)
-        
-        // Send the response data from NewsAPI to the client
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error fetching data from NewsAPI:', error);
-        res.status(500).json({ error: 'Failed to fetch data from NewsAPI' });
-    }
+    // Send the response data from NewsAPI to the client
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data from NewsAPI:", error);
+    res.status(500).json({ error: "Failed to fetch data from NewsAPI" });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`)
-})
+  console.log(`server is running on http://localhost:${PORT}`);
+});
